eb9190fcf41d27afb5916ba860dc884175f92180
Ubirajara Neves <ubirajara.neves@usp.br>
2022-08-16 18:17:50 -0300

    More components

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,9 +1,23 @@
-export default function FlashCard() {
+import { useState } from "react";
+
+export default function FlashCard({
+    title = 'Título do card',
+    description = 'Descrição do card, que pode conter mais palavras do que o título.',
+}) {
+    const [showTitle, setShowTitle] = useState(true);
+
+    function handleCardClick() {
+        setShowTitle(currentShowTitle => !currentShowTitle);
+    }
+
+    const fontSize = showTitle ? 'text-xl' : 'text-sm';
+
     return (
-        <div className={`border p-4 w-64 h-32 m-4 bg-blue-400 
-            flex flex-row items-center justify-center font-semibold text-xl`}
-            style={{fontFamily: "'JetBrains Mono', monospace"}}>
-            FlashCard
+        <div className={`shadow-lg p-4 w-80 h-48 m-4 bg-blue-400 cursor-pointer
+            flex flex-row items-center justify-center font-semibold ${fontSize}`}
+            style={{fontFamily: "'JetBrains Mono', monospace"}}
+            onClick={handleCardClick}>
+            {showTitle ? title : description}
         </div>
     );
 }
--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -1,13 +1,59 @@
+import { allFlashCards } from '../data/allFlashCards';
+import { helperShuffleArray } from '../helpers/arrayHelpers';
+import { useState } from 'react';
+
 import FlashCard from '../components/FlashCard';
 import Header from '../components/Header';
 import Main from '../components/Main';
+import FlashCards from '../components/FlashCards';
+import Button from '../components/Button';
+import RadioButton from '../components/RadioButton';
 
 export default function FlashCardsPage() {
+    const [allCards, setAllCards] = useState(allFlashCards);
+    const [showTitle, setShowTitle] = useState(true);
+
+    function handleButtonClick() {
+        const shuffledCards = helperShuffleArray(allCards);
+
+        setAllCards(shuffledCards);
+    }
+
+    function handleRadioShowDescriptionClick() {
+        setShowTitle(false);
+    }
+
+    function handleRadioShowTitleClick() {
+        setShowTitle(true);
+    }
+
     return (
         <>
             <Header>React Flash Cards - v.1</Header>
             <Main>
-                <FlashCard />
+                <div className='text-center mb-4'>
+                    <Button onButtonClick={handleButtonClick}>Embaralhar cards</Button>
+                </div>
+                <div className='flex flex-row items-center justify-center space-x-4 m-4'>
+                    <RadioButton id='radioButtonShowTitle' name='showInfo' buttonChecked={showTitle}
+                        onButtonClick={handleRadioShowTitleClick}>
+                        Mostrar título
+                    </RadioButton>
+                    <RadioButton id='radioButtonShowDescription' name='showInfo' buttonChecked={!showTitle}
+                        onButtonClick={handleRadioShowDescriptionClick}>
+                        Mostrar descrição
+                    </RadioButton>
+                </div>
+                <FlashCards>
+                    {
+                        allCards.map(({id, title, description}) => {
+                            return (
+                                <FlashCard key={id} title={title}
+                                    description={description} />
+                            );
+                        })
+                    }
+                </FlashCards>
             </Main>
         </>
     );
