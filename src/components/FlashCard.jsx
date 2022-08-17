2be6a55e4d134bc94ecb70bad372d3bc8e935dbd
Ubirajara Neves <ubirajara.neves@usp.br>
2022-08-17 11:56:16 -0300

    Uso de lifting state up.

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from "react";
-
 export default function FlashCard({
+    id,
     title = 'Título do card',
     description = 'Descrição do card, que pode conter mais palavras do que o título.',
     showFlashCardTitle = true,
+    onToggleFlashCard = null,
 }) {
-    const [showTitle, setShowTitle] = useState(showFlashCardTitle);
+    // const [showTitle, setShowTitle] = useState(showFlashCardTitle);
 
-    useEffect(() => {
-        setShowTitle(showFlashCardTitle);
-    }, [showFlashCardTitle]);
+    // useEffect(() => {
+    //     setShowTitle(showFlashCardTitle);
+    // }, [showFlashCardTitle]);
 
     function handleCardClick() {
-        setShowTitle(currentShowTitle => !currentShowTitle);
+        // setShowTitle(currentShowTitle => !currentShowTitle);
+        if (onToggleFlashCard)
+            onToggleFlashCard(id);
     }
 
-    const fontSize = showTitle ? 'text-xl' : 'text-sm';
+    const fontSize = showFlashCardTitle ? 'text-xl' : 'text-sm';
 
     return (
         <div className={`shadow-lg p-4 w-80 h-48 m-4 bg-blue-400 cursor-pointer
             flex flex-row items-center justify-center font-semibold ${fontSize}`}
             style={{fontFamily: "'JetBrains Mono', monospace"}}
             onClick={handleCardClick}>
-            {showTitle ? title : description}
+            {showFlashCardTitle ? title : description}
         </div>
     );
 }
--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -11,7 +11,7 @@ import RadioButton from '../components/RadioButton';
 
 export default function FlashCardsPage() {
     const [allCards, setAllCards] = useState(allFlashCards);
-    const [showTitle, setShowTitle] = useState(false);
+    const [radioButtonShowTitle, setRadioButtonShowTitle] = useState(true);
 
     function handleButtonClick() {
         const shuffledCards = helperShuffleArray(allCards);
@@ -20,11 +20,23 @@ export default function FlashCardsPage() {
     }
 
     function handleRadioShowDescriptionClick() {
-        setShowTitle(false);
+        const updatedCards = [...allCards].map(card => ({...card, showTitle: false}))
+        setAllCards(updatedCards);
+        setRadioButtonShowTitle(false);
     }
 
     function handleRadioShowTitleClick() {
-        setShowTitle(true);
+        const updatedCards = [...allCards].map(card => ({...card, showTitle: true}));
+        setAllCards(updatedCards);
+        setRadioButtonShowTitle(true);
+    }
+
+    function handleToggleFlashCard(cardId) {
+        const updatedCards = [...allCards];
+        const cardIndex = updatedCards.findIndex(card => card.id === cardId);
+        updatedCards[cardIndex].showTitle = !updatedCards[cardIndex].showTitle;
+
+        setAllCards(updatedCards);
     }
 
     return (
@@ -35,22 +47,32 @@ export default function FlashCardsPage() {
                     <Button onButtonClick={handleButtonClick}>Embaralhar cards</Button>
                 </div>
                 <div className='flex flex-row items-center justify-center space-x-4 m-4'>
-                    <RadioButton id='radioButtonShowTitle' name='showInfo' buttonChecked={showTitle}
+                    <RadioButton 
+                        id='radioButtonShowTitle' 
+                        name='showInfo' 
+                        buttonChecked={radioButtonShowTitle}
                         onButtonClick={handleRadioShowTitleClick}>
                         Mostrar título
                     </RadioButton>
-                    <RadioButton id='radioButtonShowDescription' name='showInfo' buttonChecked={!showTitle}
+                    <RadioButton 
+                        id='radioButtonShowDescription' 
+                        name='showInfo' 
+                        buttonChecked={!radioButtonShowTitle}
                         onButtonClick={handleRadioShowDescriptionClick}>
                         Mostrar descrição
                     </RadioButton>
                 </div>
                 <FlashCards>
                     {
-                        allCards.map(({id, title, description}) => {
+                        allCards.map(({id, title, description, showTitle}) => {
                             return (
-                                <FlashCard key={id} title={title}
+                                <FlashCard 
+                                    key={id}
+                                    id={id}
+                                    title={title}
                                     description={description}
-                                    showFlashCardTitle={showTitle} />
+                                    showFlashCardTitle={showTitle}
+                                    onToggleFlashCard={handleToggleFlashCard} />
                             );
                         })
                     }
