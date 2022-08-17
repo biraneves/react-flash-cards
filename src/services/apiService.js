374415ce775529debe74494eafce055998a2ced0
Ubirajara Neves <ubirajara.neves@usp.br>
2022-08-17 14:44:01 -0300

    Criado o arquivo httpService.js e implementada a função get.

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,3 +1,9 @@
-export function apiGetAllFlashCards() {
-    return [];
+import { get } from "./httpService";
+
+const BACK_END_URL = 'http://localhost:3004/flashcards';
+
+export async function apiGetAllFlashCards() {
+    const allFlashCards = await get(BACK_END_URL);
+
+    return allFlashCards;
 }
---
+++ src/services/httpService.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export async function get(url) {
+    const { data } = await axios.get(url);
+
+    return data;
+}
